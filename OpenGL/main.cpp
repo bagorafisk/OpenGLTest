545040d6326e42da5643ac43c5525c96e3be2e45
Bagorafisk <81960941+bagorafisk@users.noreply.github.com>
2024-11-14 21:08:26 +0100

    dingus

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -2,9 +2,73 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include "Shader.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb/stb_image.h"
+
+void drawRectangle(float x, float y, float width, float height, glm::vec3 color, Shader& shader) {
+	// Define vertices based on position and size
+	float vertices[] = {
+		// positions          // colors       // texture coords
+		 x + width, y + height, 0.0f, color.r, color.g, color.b, 1.0f, 1.0f, // top right
+		 x + width, y,          0.0f, color.r, color.g, color.b, 1.0f, 0.0f, // bottom right
+		 x,         y,          0.0f, color.r, color.g, color.b, 0.0f, 0.0f, // bottom left
+		 x,         y + height, 0.0f, color.r, color.g, color.b, 0.0f, 1.0f  // top left
+	};
+	unsigned int indices[] = {
+		0, 1, 3,  // first triangle
+		1, 2, 3   // second triangle
+	};
+
+	// Generate and bind buffers (for one-time setup)
+	unsigned int VAO, VBO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// Position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// Color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	// Texture coordinate attribute
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+	// Use the shader program
+	shader.use();
+
+	// Bind the texture (if any)
+	// glActiveTexture(GL_TEXTURE0);
+	// glBindTexture(GL_TEXTURE_2D, texture);  // Bind texture if needed
+
+	// Set any uniforms (e.g., for transformations) if needed
+
+	// Draw the rectangle
+	glBindVertexArray(VAO);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+	// Unbind and cleanup (optional in some cases)
+	glBindVertexArray(0);
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+}
+
 
 float vertices[] = {
 	// positions          // colors           // texture coords
@@ -25,6 +89,9 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 void processInput(GLFWwindow* window) {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
 		glfwSetWindowShouldClose(window, true);
+	}
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+		vertices[0] += 0.1f;
 	}
 }
 
@@ -66,7 +133,7 @@ int main() {
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_DYNAMIC_DRAW);
 
 	// Position attribute
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
@@ -111,6 +178,8 @@ int main() {
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
+	stbi_set_flip_vertically_on_load(true);
+
 	data = stbi_load("assets/awesomeface.png", &width, &height, &nrChannels, 0);
 	if (data)
 	{
@@ -126,6 +195,7 @@ int main() {
 	shaderProgram.use();
 	shaderProgram.setInt("texture1", 0);
 	shaderProgram.setInt("texture2", 1);
+
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -144,10 +214,19 @@ int main() {
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, texture2);
 
+		glm::mat4 trans = glm::mat4(1.0f);
+		trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
+		trans = glm::rotate(trans, (float)0, glm::vec3(0.0f, 0.0f, 1.0f));
+
+		 int transformLoc = glGetUniformLocation(shaderProgram.ID, "transform");
+		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+
 		// render container
 		shaderProgram.use();
 		glBindVertexArray(VAO);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		drawRectangle(0.0f, 0.0f, 1.0f, 1.0f, glm::vec3(1.0f, 1.0f, 1.0f), shaderProgram);
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		// -------------------------------------------------------------------------------
